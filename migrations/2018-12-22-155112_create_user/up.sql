3d37c3041b3de6e3a4c80cdc002f3268483c88ed
Rien Maertens <rien.maertens@posteo.be>
2019-10-30 17:14:05 +0100

    Merge migrations into schema

--- migrations/2018-12-22-155112_create_user/up.sql
+++
@@ -1,7 +0,0 @@
--- Your SQL goes here
-CREATE TABLE user (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    username VARCHAR(127) NOT NULL,
-    password VARCHAR(127) NOT NULL,
-    admin    BOOLEAN NOT NULL DEFAULT 0
-);
---
+++ migrations/2019-10-30-155718_schema/down.sql
@@ -0,0 +1,2 @@
+drop table user;
+drop table client;
