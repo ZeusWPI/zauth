c559e927087df8245d57a50c7f9d60853326149d
Rien Maertens <rien.maertens@posteo.be>
2020-01-19 17:41:49 +0100

    Create 'Accepter'
    Which responds according to the format requested in the 'Accept' header.
    Kind of like how Ruby-on-Rails' `format` works.

--- src/views/view.rs
+++ src/views/view.rs
@@ -1,6 +1,6 @@
-use rocket::http::{QMediaType, Status};
+use rocket::http::{MediaType, QMediaType};
 use rocket::request::Request;
-use rocket::response::{self, Responder, Response};
+use rocket::response::{self, Responder};
 
 #[macro_export]
 macro_rules! template {
@@ -33,91 +33,166 @@ macro_rules! template {
 	}
 }
 
-type FnRespond<'r> = Box<dyn FnOnce(&Request) -> response::Result<'r>>;
-
-pub struct View<'r> {
-	json: Option<FnRespond<'r>>,
-	html: Option<FnRespond<'r>>,
+pub struct Accepter<H, J, D> {
+	html:    H,
+	json:    J,
+	default: D,
 }
 
-impl<'r> View<'r> {
-	pub fn new() -> Self {
-		View {
-			json: None,
-			html: None,
-		}
-	}
-
-	pub fn json(mut self, respond: FnRespond<'r>) -> Self {
-		self.json = Some(respond);
-		self
-	}
-
-	pub fn html(mut self, respond: FnRespond<'r>) -> Self {
-		self.html = Some(respond);
-		self
-	}
+fn preferred_media<'r>(request: &'r Request) -> Vec<&'r MediaType> {
+	request
+		.accept()
+		.map(|accept| {
+			let mut accepts = accept.iter().collect::<Vec<&QMediaType>>();
+			accepts.sort_by(|p, q| {
+				let pw = p.weight_or(1.0);
+				let qw = q.weight_or(1.0);
+				qw.partial_cmp(&pw).unwrap_or(std::cmp::Ordering::Less)
+			});
+			accepts
+				.iter()
+				.map(|qmedia| qmedia.media_type())
+				.collect::<Vec<&'r MediaType>>()
+		})
+		.unwrap_or_else(Vec::new)
 }
 
-impl<'r> Responder<'r> for View<'r> {
+fn media_types_match(first: &MediaType, other: &MediaType) -> bool {
+	let collide = |a, b| a == "*" || b == "*" || a == b;
+	collide(first.top(), other.top()) && collide(first.sub(), other.sub())
+}
+
+impl<'r, H: Responder<'r>, J: Responder<'r>, D: Responder<'r>> Responder<'r>
+	for Accepter<H, J, D>
+{
 	fn respond_to(self, request: &Request) -> response::Result<'r> {
-		request
-			.accept()
-			.and_then(|accept| {
-				let mut accepts = accept.iter().collect::<Vec<&QMediaType>>();
-				accepts.sort_by(|p, q| {
-					let pw = p.weight_or(1.0);
-					let qw = q.weight_or(1.0);
-					pw.partial_cmp(&qw).unwrap_or(std::cmp::Ordering::Less)
-				});
-				for qmedia in accepts {
-					if qmedia.is_json() {
-						return self.json;
-					} else if qmedia.is_html() {
-						return self.html;
-					}
-				}
-				None
-			})
-			.map(|respond| respond(request))
-			.unwrap_or_else(|| {
-				let template = template!("errors/406").respond_to(request)?;
-				Ok(Response::build_from(template)
-					.status(Status::NotAcceptable)
-					.finalize())
-			})
+		for media in preferred_media(request) {
+			if media_types_match(media, &MediaType::HTML) {
+				return self.html.respond_to(request);
+			} else if media_types_match(media, &MediaType::JSON) {
+				return self.json.respond_to(request);
+			}
+		}
+		return self.default.respond_to(request);
 	}
 }
 
 #[cfg(test)]
 mod test {
 	use super::*;
-	use rocket::http::Status;
+	use rocket::http::{Accept, Header, MediaType, Status};
 	use rocket::local::Client;
+	use rocket::response::content::Html;
+	use rocket::response::status::Custom;
+	use rocket_contrib::json::Json;
 	use rocket_contrib::templates::Template;
 
-	#[get("/nothing")]
-	fn view_nothing<'r>() -> View<'r> {
-		View::new()
+	#[get("/test")]
+	fn test_view() -> Accepter<
+		Html<&'static str>,
+		Json<Vec<&'static str>>,
+		Custom<&'static str>,
+	> {
+		Accepter {
+			json:    Json(vec!["hello json"]),
+			html:    Html("<html><h1>Hello HTML"),
+			default: Custom(Status::NotAcceptable, "not acceptable"),
+		}
 	}
 
 	fn client() -> Client {
 		Client::new(
 			rocket::ignite()
 				.attach(Template::fairing())
-				.mount("/", routes![view_nothing]),
+				.mount("/", routes![test_view]),
 		)
 		.unwrap()
 	}
 
 	#[test]
-	fn test_nothing() {
+	fn accept_html() {
 		let client = client();
-		let mut response = client.get("/nothing").dispatch();
-		assert_eq!(response.status(), Status::NotAcceptable);
+		let mut response = client.get("/test").header(Accept::HTML).dispatch();
+		assert_eq!(response.status(), Status::Ok);
+		assert_eq!(
+			response.headers().get("content-type").next(),
+			Some("text/html; charset=utf-8")
+		);
 		assert!(response
 			.body_string()
 			.expect("html body")
-			.contains("<h1>Not acceptable</h1>"));
+			.contains("Hello HTML"));
+	}
+
+	#[test]
+	fn accept_json() {
+		let client = client();
+		let mut response = client.get("/test").header(Accept::JSON).dispatch();
+		assert_eq!(response.status(), Status::Ok);
+		assert_eq!(
+			response.headers().get("content-type").next(),
+			Some("application/json")
+		);
+		assert_eq!(
+			response.body_string().expect("json body"),
+			"[\"hello json\"]"
+		);
+	}
+
+	#[test]
+	fn not_acceptable() {
+		let client = client();
+		let response = client.get("/test").header(Accept::XML).dispatch();
+		assert_eq!(response.status(), Status::NotAcceptable);
+	}
+
+	#[test]
+	fn accept_anything() {
+		let client = client();
+		let response = client.get("/test").header(Accept::Any).dispatch();
+		assert_eq!(response.status(), Status::Ok);
+	}
+
+	#[test]
+	fn preffers_html() {
+		let client = client();
+		let mut response = client
+			.get("/test")
+			.header(Header::new(
+				"Accept",
+				"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;\
+				 q=0.8",
+			))
+			.dispatch();
+		assert_eq!(response.status(), Status::Ok);
+		assert_eq!(
+			response.headers().get("content-type").next(),
+			Some("text/html; charset=utf-8")
+		);
+		assert!(response
+			.body_string()
+			.expect("html body")
+			.contains("Hello HTML"));
+	}
+
+	#[test]
+	fn preffers_json() {
+		let client = client();
+		let mut response = client
+			.get("/test")
+			.header(Header::new(
+				"Accept",
+				"bloep/bliep;q=0.9,application/json;q=0.8,*/*;q=0.8",
+			))
+			.dispatch();
+		assert_eq!(response.status(), Status::Ok);
+		assert_eq!(
+			response.headers().get("content-type").next(),
+			Some("application/json")
+		);
+		assert_eq!(
+			response.body_string().expect("json body"),
+			"[\"hello json\"]"
+		);
 	}
 }
