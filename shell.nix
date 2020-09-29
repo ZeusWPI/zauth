247205bc18ad291ce7b50a2c909f84cf5801e273
Francis Begyn <francis@begyn.be>
2020-09-29 23:13:51 +0200

    shell.nix - workaround for missing rls

--- shell.nix
+++ shell.nix
@@ -1,13 +1,20 @@
 let
   sources = import ./nix/sources.nix;
   pkgs = import sources.nixpkgs { overlays = [ (import sources.nixpkgs-mozilla) ]; };
-  rustnightly = pkgs.latest.rustChannels.stable.rust.override {
+  rustNightly = pkgs.latest.rustChannels.nightly.rust.override {
+    extensions = [ "rustfmt-preview" "rls-preview" ];
+  };
+  # the rls is missing on the nightly build of 2020-09-29, so pinning a date for the moment
+  rustNightlyTemp = (pkgs.rustChannelOf{
+    date = "2020-09-28";
+    channel = "nightly";
+  }).rust.override {
     extensions = [ "rustfmt-preview" "rls-preview" ];
   };
 in
 pkgs.mkShell {
   buildInputs = [
-    rustnightly
+    rustNightlyTemp
     pkgs.postgresql
     pkgs.pgcli
   ];
