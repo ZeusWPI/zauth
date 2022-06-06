f99b81618b0808e54768416d7975d594d2cf67a5
Charlotte Van Petegem <charlotte.vanpetegem@ugent.be>
2022-06-06 16:51:45 +0200

    Also add a comment in `clients_controller`

--- src/controllers/clients_controller.rs
+++ src/controllers/clients_controller.rs
@@ -15,6 +15,13 @@ use crate::models::user::User;
 use crate::views::accepter::Accepter;
 use crate::DbConn;
 
+
+// These structs need to be defined separately. Because we use a hidden field in
+// the HTML to make sure we always get some value for `needs_grant`, we need
+// Rocket to be lenient when parsing the form data. However, the `Lenient`
+// struct does not play nice with any other libraries. (So it can't be
+// deserialized by serde.)
+
 #[derive(Deserialize, Debug)]
 pub struct JsonClientChange {
 	pub name:              Option<String>,
